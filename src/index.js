75873f652d9f2d300e557171074cfd8d084f8319
Amospikins <sanmco2013@gmail.com>
2024-03-25 23:40:07 +0100

    added custom control/title bar to authentication pages with theme changes

--- src/index.js
+++ src/index.js
@@ -1,57 +1,53 @@
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
+const { app, BrowserWindow } = require('electron');
+const { registerIPCMainHandlers } = require('./ipcHandlers');
 const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
-//setupTitlebar();
 
 const createSplashWindow = () => {
-  // Create the browser window.
   const splashWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
     frame: false,
     alwaysOnTop: true,
     center: true,
     resizable: false,
-    skipTaskbar: true, // Set skipTaskbar to true
+    skipTaskbar: true
   });
   splashWindow.loadFile(path.join(__dirname, 'splash.html'));
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
   return splashWindow;
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-  const splashWindow = createSplashWindow();
-
-  /// Create your mainWindow here
+const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
+    frame: false,
     width: 800,
     height: 600,
+    minWidth: 600,
+    minHeight: 600,
     webPreferences: {
-      //  devTools: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
+      devTools: false, // Disable DevTools
+      preload: path.join(__dirname, 'preload.js')
+    }
   });
   mainWindow.setIcon(path.join(__dirname, '/assets/images/logo_with_bg.png'));
-  mainWindow.loadFile(path.join(__dirname, 'screens/authentication/login.html'));
+  mainWindow.loadFile(path.join(__dirname, '/screens/authentication/login.html'));
 
+  return mainWindow;
+};
+
+app.on('ready', () => {
+  const splashWindow = createSplashWindow();
+  const mainWindow = createMainWindow();
   mainWindow.once('ready-to-show', () => {
     splashWindow.destroy();
     mainWindow.show();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -59,26 +55,13 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createMainWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-/*electron-forge start*/
 
-ipcMain.handle('dark-mode:toggle', () => {
-  if (nativeTheme.shouldUseDarkColors) {
-    nativeTheme.themeSource = 'light'
-  } else {
-    nativeTheme.themeSource = 'dark'
-  }
-  return nativeTheme.shouldUseDarkColors
-})
 
-ipcMain.handle('dark-mode:system', () => {
-  nativeTheme.themeSource = 'system'
-});
+
+// Handle IPC messages for window actions
+registerIPCMainHandlers();
--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,20 @@
+
+
+//  FOR CHANGING THEME
 document.getElementById('darkmode-toggle').addEventListener('click', async () => {
   const isDarkMode = await window.darkMode.toggle()
 })
 document.getElementById('reset-to-system').addEventListener('click', async () => {
   await window.darkMode.system()
 })
+
+
+document.getElementById('minimize-button').addEventListener('click', async () => {
+  await window.electronAPI.minimizeWindow();
+});
+document.getElementById('maximize-button').addEventListener('click', async () => {
+  await window.electronAPI.maximizeWindow();
+});
+document.getElementById('close-button').addEventListener('click', async () => {
+  await window.electronAPI.closeWindow();
+});
