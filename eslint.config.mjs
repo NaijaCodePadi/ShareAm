8d040f16546710d1087098a1c139dc9379b7fcb3
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-05-06 21:08:37 +0100

    trying to set up eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
