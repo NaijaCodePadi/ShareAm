a4d7fcf08948b5f89e24334ee254af7755734259
Adeyemi Eric Ademola <119066166+Ericademola@users.noreply.github.com>
2024-06-22 23:48:56 +0100

    add files to hold variables

---
+++ src/variables/mock_variables/mock_dashboard.js
@@ -0,0 +1,38 @@
+export const chatSample = [
+    {
+        image: "../../assets/images/user 3.png",
+        name: "",
+        varified: false,
+        username: "@starface",
+    },
+    {
+        image: "",
+        name: "Osita Jerry",
+        varified: true,
+        username: "@tomjerry",
+    },
+    {
+        image: "../../assets/images/user 1.png",
+        name: "NikiJ",
+        varified: true,
+        username: "@juijui",
+    },
+    {
+        image: "../../assets/images/user 2.png",
+        name: "Blessing Thomas",
+        varified: false,
+        username: "@idoubt",
+    },
+    {
+        image: "../../assets/images/user 4.png",
+        name: "Osato Zoe",
+        varified: true,
+        username: "@zoe!",
+    },
+    {
+        image: "../../assets/images/user 3.png",
+        name: "",
+        varified: false,
+        username: "@olomi",
+    },
+]
