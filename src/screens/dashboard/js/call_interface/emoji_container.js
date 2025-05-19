8899ed5fdedc71cd9314652b12b16bbe4d833c9a
Juliet-niki <nwajully@gmail.com>
2025-05-19 22:46:12 +0100

    emoji picker

--- src/screens/dashboard/js/call_interface/emoji_container.js
+++ src/screens/dashboard/js/call_interface/emoji_container.js
@@ -1,5 +1,7 @@
 import { emojiCategories } from "../../../../variables/mock_variables/mock_emoji.js";
 
+const emojiOverlay = document.getElementById("emoji-btn");
+const emojiOverlayWrapper = document.getElementById("emoji-overlay-wrapper");
 const emojiTabContent = document.querySelectorAll(".emoji-tab-content");
 const tabEmojis = document.querySelectorAll(".tab-emoji");
 const getPeopleEmoji = document.getElementById("people");
@@ -10,6 +12,38 @@ const getTravelEmoji = document.getElementById("travel");
 const getObjectsEmoji = document.getElementById("objects");
 const getSymbolsEmoji = document.getElementById("symbols");
 const getFlagsEmoji = document.getElementById("flags");
+
+// -------- EMOJI OVERLAY ----------- //
+emojiOverlay.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (window.getComputedStyle(emojiOverlayWrapper).display === "none") {
+    emojiOverlayWrapper.style.display = "block";
+  } else {
+    emojiOverlayWrapper.style.display = "none";
+  }
+});
+
+emojiOverlayWrapper.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    window.getComputedStyle(emojiOverlayWrapper).display === "block" &&
+    !emojiOverlayWrapper.contains(event.target) &&
+    !emojiOverlay.contains(event.target)
+  ) {
+    tabEmojis.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tabEmojis[0].classList.add("active");
+    emojiTabContent.forEach((content) => {
+      content.classList.remove("active");
+    });
+    emojiTabContent[0].classList.add("active");
+    emojiOverlayWrapper.style.display = "none";
+  }
+});
 
 //-------- EMOJI CATEGORY TAB --------//
 const handleEmojiTabs = () => {
@@ -36,72 +70,56 @@ const handleEmojiCategoryDisplay = () => {
       item.emojis.forEach((person) => {
         const peopleEmoji = document.createElement("span");
         peopleEmoji.classList.add("emoji");
-        peopleEmoji.innerHTML = `
-            ${person}
-          `;
+        peopleEmoji.innerHTML = person;
         getPeopleEmoji.appendChild(peopleEmoji);
       });
     } else if (item.title === "Animals & Nature") {
       item.emojis.forEach((animal) => {
         const animalEmoji = document.createElement("span");
         animalEmoji.classList.add("emoji");
-        animalEmoji.innerHTML = `
-            ${animal}
-          `;
+        animalEmoji.innerHTML = animal;
         getAnimalsEmoji.appendChild(animalEmoji);
       });
     } else if (item.title === "Food & Drink") {
       item.emojis.forEach((food) => {
         const foodEmoji = document.createElement("span");
         foodEmoji.classList.add("emoji");
-        foodEmoji.innerHTML = `
-            ${food}
-          `;
+        foodEmoji.innerHTML = food;
         getFoodEmoji.appendChild(foodEmoji);
       });
     } else if (item.title === "Activity & Sports") {
       item.emojis.forEach((activity) => {
         const activityEmoji = document.createElement("span");
         activityEmoji.classList.add("emoji");
-        activityEmoji.innerHTML = `
-            ${activity}
-          `;
+        activityEmoji.innerHTML = activity;
         getActivityEmoji.appendChild(activityEmoji);
       });
     } else if (item.title === "Travel & Places") {
       item.emojis.forEach((travel) => {
         const travelEmoji = document.createElement("span");
         travelEmoji.classList.add("emoji");
-        travelEmoji.innerHTML = `
-            ${travel}
-          `;
+        travelEmoji.innerHTML = travel;
         getTravelEmoji.appendChild(travelEmoji);
       });
     } else if (item.title === "Objects") {
       item.emojis.forEach((object) => {
         const objectEmoji = document.createElement("span");
         objectEmoji.classList.add("emoji");
-        objectEmoji.innerHTML = `
-            ${object}
-          `;
+        objectEmoji.innerHTML = object;
         getObjectsEmoji.appendChild(objectEmoji);
       });
     } else if (item.title === "Symbols") {
       item.emojis.forEach((symbol) => {
         const symbolEmoji = document.createElement("span");
         symbolEmoji.classList.add("emoji");
-        symbolEmoji.innerHTML = `
-            ${symbol}
-          `;
+        symbolEmoji.innerHTML = symbol;
         getSymbolsEmoji.appendChild(symbolEmoji);
       });
     } else if (item.title === "Flags") {
       item.emojis.forEach((flag) => {
         const flagEmoji = document.createElement("span");
         flagEmoji.classList.add("emoji");
-        flagEmoji.innerHTML = `
-            ${flag}
-          `;
+        flagEmoji.innerHTML = flag;
         getFlagsEmoji.appendChild(flagEmoji);
       });
     }
@@ -109,3 +127,26 @@ const handleEmojiCategoryDisplay = () => {
 };
 
 handleEmojiCategoryDisplay();
+
+//-------- EMOJI CLICK --------//
+const emojiBoxes = document.querySelectorAll(".emoji-box");
+const formInput = document.getElementById("messages");
+
+const handleEmojiPicker = () => {
+  emojiBoxes.forEach((box) => {
+    box.addEventListener("click", function (e) {
+      if (e.target.classList.contains("emoji")) {
+        const emoji = e.target.textContent;
+        const cursorPos = formInput.selectionStart;
+        const textBefore = formInput.value.substring(0, cursorPos);
+        const textAfter = formInput.value.substring(cursorPos);
+        formInput.value = textBefore + emoji + textAfter;
+        formInput.focus();
+        formInput.selectionStart = formInput.selectionEnd =
+          cursorPos + emoji.length;
+      }
+    });
+  });
+};
+
+handleEmojiPicker();
