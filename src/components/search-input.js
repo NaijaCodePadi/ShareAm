d81cb77a10f95f51af348d03377d19ead81a9458
RUDYMAZE <njockrudiger4@gmail.com>
2025-05-23 19:06:54 +0300

    debounce componenent completed and exported and called in search-input.js to initialize

--- src/components/search-input.js
+++ src/components/search-input.js
@@ -1,20 +1,3 @@
-// const inputText = document.getElementById("debounce-text");
-// const updateDebounceText = debounce((inputText) => {
-//   console.log(inputText);
-// });
-// inputText.addEventListener("input", () => {
-//   updateDebounceText(inputText.value);
-// });
-// function debounce(text, delay = 1500) {
-//   let timeout;
-//   return (...args) => {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(() => {
-//       text(...args);
-//     }, delay);
-//   };
-// }
-
 import { DebounceInput } from "./debounce-comp.js";
 
 const searchInput = document.getElementById("debounce-text");
@@ -26,7 +9,5 @@ const debouncedSearch = new DebounceInput({
   },
   delay: 1500,
 });
-
-debouncedSearch.setDelay(500);
 
 debouncedSearch.destroy();
